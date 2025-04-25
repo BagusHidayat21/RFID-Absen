92c1f2691ede78c6e5db93e92024828add17ba44
JustThiriq <mthoriq2076@gmail.com>
2025-04-25 09:31:25 +0700

    update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,12 +2,11 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 // Koneksi database menggunakan Pool
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 // Setup database dan tabel
